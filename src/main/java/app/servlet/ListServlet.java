cfe12dfbab41042654c77a9d35cdd6ddea5bd046
Egor Duvalov <happytosiu@gmail.com>
2021-02-16 11:16:47 +0300

    Import changed

--- src/main/java/app/servlet/ListServlet.java
+++ src/main/java/app/servlet/ListServlet.java
@@ -1,17 +1,16 @@
 package app.servlet;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher dispatcher = req.getRequestDispatcher("views/list.jsp");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("views/add.jsp");
         dispatcher.forward(req, resp);
     }
 }
